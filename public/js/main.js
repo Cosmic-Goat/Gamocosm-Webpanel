0ea90c78bc9d464772f0afd7737015e9813a92e6
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-10-07 17:41:35 -0400

    Created initial webpage.

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+ document.addEventListener('DOMContentLoaded', function () {
+     var elems = document.querySelectorAll('.parallax');
+     var instances = M.Parallax.init(elems);
+
+
+     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+     // // The Firebase SDK is initialized and available here!
+     //
+     // firebase.auth().onAuthStateChanged(user => { });
+     // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+     // firebase.messaging().requestPermission().then(() => { });
+     // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+     //
+     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+
+ });
